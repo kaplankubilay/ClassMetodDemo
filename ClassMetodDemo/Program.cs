91c3cf994fac99d705ccc6f068bf54f5b9532578
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-05-01 12:35:34 +0300

    Add project files.

---
+++ ClassMetodDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassMetodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
